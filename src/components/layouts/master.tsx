230e20284081cb3c1ee5c77b24100d68e4a7eb8f
fiwz <afianiii@gmail.com>
2024-07-14 23:06:46 +0700

    fix: move activity card component, show list from local storage

--- src/components/layouts/master.tsx
+++ src/components/layouts/master.tsx
@@ -1,18 +1,16 @@
 import Navigation from "./navigation";
 import Section from "../section";
-import ActivityCard from "../card";
+// import ActivityCard from "../card";
 import { ActivityForm } from "../forms/activity-form";
 
-const Master = ({ children }) => {
+const Master = ({ children }: any) => {
   return (
     <>
       <div className="container mx-auto mb-10 pb-5 w-screen">
         {children}
         <Navigation></Navigation>
         <ActivityForm></ActivityForm>
-        <Section>
-          <ActivityCard></ActivityCard>
-        </Section>
+        <Section>{/* <ActivityCard></ActivityCard> */}</Section>
       </div>
     </>
   );
