3f171551e990ae481347fe8a1c39fc8e080b79ae
fiwz <afianiii@gmail.com>
2024-07-14 18:27:16 +0700

    style: add badge style

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,5 +1,10 @@
 import { TagIcon } from "@heroicons/react/24/solid";
 
+function getBadgeClass() {
+  const tagClasses = ["", "badge-primary", "badge-secondary", "badge-accent"];
+  return tagClasses[Math.floor(Math.random() * tagClasses.length)];
+}
+
 const ActivityCard = () => {
   return (
     <>
@@ -7,9 +12,26 @@ const ActivityCard = () => {
         <div className="card-body">
           <h2 className="card-title">Lari Pagi</h2>
           <p>Hwaiting!!</p>
-          <div className="flex content-center">
+          <div className="flex content-center flex-wrap gap-1">
             <TagIcon className="size-6 text-secondary pt-1" />
-            <p>Tags: sports, habbit</p>
+            <div className={`badge badge-outline ${getBadgeClass()}`}>
+              default
+            </div>
+            <div
+              className={`badge badge-primary badge-outline ${getBadgeClass()}`}
+            >
+              primary
+            </div>
+            <div
+              className={`badge badge-secondary badge-outline ${getBadgeClass()}`}
+            >
+              secondary
+            </div>
+            <div
+              className={`badge badge-accent badge-outline ${getBadgeClass()}`}
+            >
+              accent
+            </div>
           </div>
           <div className="card-actions justify-end mt-5">
             <button className="btn btn-sm btn-primary">Mark as Complete</button>
