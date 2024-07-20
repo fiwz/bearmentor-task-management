a0f012d30364d6b74dbbe752fb6b73077f7496cc
fiwz <afianiii@gmail.com>
2024-07-20 12:39:07 +0700

    style: update card style

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -34,29 +34,29 @@ const ActivityCard = ({ activities }: any) => {
         activities.map((activity: any) => (
           <div key={activity.id} className="card bg-base-100 w-auto shadow-xl">
             <div className="card-body">
-              <h2 className="card-title">{activity.name}</h2>
-              <p className="text-secondary text-sm">
+              <h2 className="card-title text-base">{activity.name}</h2>
+              <p className="text-secondary text-xs">
                 Start Date:{" "}
                 {dayjs(activity.startDate)
                   .format("dddd, MMMM D, YYYY h:mm A")
                   .toString()}
               </p>
-              <p className="text-secondary text-sm">
+              <p className="text-secondary text-xs">
                 End Date:{" "}
                 {dayjs(activity.endDate)
                   .format("dddd, MMMM D, YYYY h:mm A")
                   .toString()}
               </p>
-              <p>{activity.description}</p>
+              <p className="text-sm pb-3">{activity.description}</p>
 
               <div className="flex content-center flex-wrap gap-1">
                 {activity.tags && activity.tags.length > 0 && (
                   <>
-                    <TagIcon className="size-6 text-secondary pt-1" />
+                    <TagIcon className="size-5 text-secondary" />
                     {activity.tags.map((tag: string, key: any) => (
                       <div
                         key={key}
-                        className={`badge badge-outline ${getBadgeClass()}`}
+                        className={`badge badge-outline ${getBadgeClass()} text-xs`}
                       >
                         {tag}
                       </div>
@@ -68,20 +68,20 @@ const ActivityCard = ({ activities }: any) => {
               <div className="card-actions justify-end mt-5">
                 {activity.status === 1 && (
                   <button
-                    className="btn btn-sm btn-primary"
+                    className="btn btn-sm btn-primary text-xs"
                     onClick={() => ctxValue?.markCompleteActivity(activity.id)}
                   >
                     Mark as Complete
                   </button>
                 )}
                 <button
-                  className="btn btn-sm btn-secondary"
+                  className="btn btn-sm btn-secondary text-xs"
                   onClick={() => ctxValue?.deleteActivity(activity.id)}
                 >
                   Delete
                 </button>
                 <button
-                  className="btn btn-sm btn-secondary"
+                  className="btn btn-sm btn-secondary text-xs"
                   onClick={() => ctxValue?.showActivity(activity.id)}
                 >
                   Update
